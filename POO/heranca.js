4dd73d1ba43d5320c403db0ce328da8f5963fb85
Feliphe S. Blatt <felipheblatt@gmail.com>
2024-09-02 16:19:02 -0300

    Exercise: Herança

---
+++ POO/heranca.js
@@ -0,0 +1,34 @@
+// Produto: aumento, desconto
+function Produto(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+Produto.prototype.aumento = function(porcentagem){
+    this.preco += this.preco * (porcentagem * 0.01);
+};
+Produto.prototype.desconto = function(porcentagem){
+    this.preco -= this.preco * (porcentagem * 0.01);
+};
+
+function Caneca(nome, preco, material){
+    Produto.call(this, nome, preco);
+    this.material = material;
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+
+Caneca.prototype.constructor = Caneca;
+
+const caneca = new Caneca('xicara', 100, 'porcelana');
+console.log(caneca.nome, caneca.preco, caneca.material);
+
+caneca.aumento(10);
+console.log(caneca.nome, caneca.preco, caneca.material);
+
+
+// Sobrescrevendo original do pai
+Caneca.prototype.aumento = function (valor) {
+    this.preco = 0;
+};
+caneca.aumento(10);
+console.log(caneca.nome, caneca.preco, caneca.material);
